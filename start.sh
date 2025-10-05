8724c29ad23a12ccfdb9ecd2fed1836c7c7e10d0
fghhfgtggh <newforyou175@Outlook.com>
2025-10-05 18:35:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# 后台启动你的服务
 ./or &
-sleep 2
-exec socat TCP-LISTEN:${PORT},fork TCP:127.0.0.1:6324
+
+# 等待服务启动
+sleep 3
+
+# 使用 nc (netcat) 做端口转发（Render 自带）
+while true; do
+    nc -l -p ${PORT} -c "nc localhost 6324"
+done
